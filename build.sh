30a7a44851262eb2076a6a1690bef1cc77956d75
Nntazho <gemjeruknipos@gmail.com>
2025-11-21 09:02:26 +0000

    1

--- build.sh
+++ build.sh
@@ -1,40 +1,64 @@
 #!/bin/bash
+SECONDS=0
+set -e
 
-function compile() 
-{
-rm -rf AnyKernel
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
-export ARCH=arm64
-export KBUILD_BUILD_HOST=neolit
-export KBUILD_BUILD_USER="sarthakroy2002"
-git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
+# Set kernel path
+KERNEL_PATH=out/arch/arm64/boot
 
-make O=out ARCH=arm64 RMX2020_defconfig
+# Set kernel name
+BUILD_TYPE="SUKISU"
+DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
+KERNEL_NAME="SukiSuA16${BUILD_TYPE}-${DATE}.zip"
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-androideabi-" \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+function KERNEL_COMPILE() {
+	if [ "$1" == "install" ]; then
+		# Download required package
+		sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc ccache bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y && sudo apt install llvm -y && sudo apt-get install g++-aarch64-linux-gnu -y && sudo apt install libelf-dev -y && sudo apt install neofetch -y && neofetch
+	fi
+
+	# Set environment variables
+	export USE_CCACHE=1
+	export KBUILD_BUILD_HOST=builder
+	export KBUILD_BUILD_USER=Nntazho
+
+	# Create output directory and do a clean build
+	rm -rf out && mkdir -p out
+
+	# Download clang if not present
+	if [[ ! -d "clang" ]]; then mkdir -p clang
+		wget https://github.com/Impqxr/aosp_clang_ci/releases/download/13289611/clang-13289611-linux-x86.tar.xz -O clang.tar.gz
+		tar -xf clang.tar.gz -C clang && if [ -d clang/clang-* ]; then mv clang/clang-*/* clang; fi && rm -rf clang.tar.gz
+	fi
+
+	# Add clang bin directory to PATH
+	export PATH="${PWD}/clang/bin:$PATH"
+
+	# Make the config
+	make O=out ARCH=arm64 RMX2020_defconfig
+
+	# Build the kernel with clang and log output
+	make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_COMPAT=arm-linux-gnueabi- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1 2>&1 | tee -a out/compile.log
 }
 
-function zupload()
-{
-rm -rf AnyKernel
-git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
-cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-cd AnyKernel
-zip -r9 Test-OSS-KERNEL-RMX2020-NEOLIT.zip *
-curl -sL https://git.io/file-transfer | sh
-./transfer wet Test-OSS-KERNEL-RMX2020-NEOLIT.zip
+function KERNEL_RESULT() {
+	# Create anykernel
+	rm -rf anykernel
+	git clone https://github.com/sarthakroy2002/AnyKernel3.git anykernel
+
+        #cp
+        cp out/arch/arm64/boot/Image.gz-dtb anykernel
+
+	# Created zip kernel
+	cd anykernel && zip -r9 "${KERNEL_NAME}" *
+
+	# Upload kernel
+	RESPONSE=$(curl -s -F "file=@${KERNEL_NAME}" "https://store1.gofile.io/contents/uploadfile" \
+	|| curl -s -F "file=@${KERNEL_NAME}" "https://store2.gofile.io/contents/uploadfile")
+	DOWNLOAD_LINK=$(echo "$RESPONSE" | grep -oP '"downloadPage":"\K[^"]+')
+	echo -e "\nDownload link: $DOWNLOAD_LINK"
 }
 
-compile
-zupload
+# Run functions
+KERNEL_COMPILE "$1"
+KERNEL_RESULT
+echo -e "Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !\n"
