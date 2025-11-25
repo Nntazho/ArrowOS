c9e3237dafa268b3e504f7134f1814a7a053f384
Nntazho <gemjeruknipos@gmail.com>
2025-11-25 13:53:43 +0800

    Ksu NEXT

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
 KERNEL_NAME="Rk${BUILD_TYPE}-${DATE}.zip"
 
 # Clone SukiSU repo
-if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s susfs-rksu-master; fi
+if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/kernel-build-from-rainyland/KernelSU-Next/next/kernel/setup.sh" | bash -s next; fi
 
 function KERNEL_COMPILE() {
 	# Set environment variables
