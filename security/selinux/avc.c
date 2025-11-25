fc9ec93fcd94ef29a694ca3b194877d52cb858d2
Nntazho <gemjeruknipos@gmail.com>
2025-11-25 12:29:17 +0800

    y

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -156,7 +156,7 @@ static void avc_dump_query(struct audit_buffer *ab, u32 ssid, u32 tsid, u16 tcla
 	char *scontext;
 	u32 scontext_len;
 
-	rc = security_sid_to_context(ssid, &scontext, &scontext_len);
+	rc = security_sid_to_context(state, ssid, &scontext, &scontext_len);
 	if (rc)
 		audit_log_format(ab, "ssid=%d", ssid);
 	else {
